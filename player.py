ff6a2aa197c386df4728d8878ae5a02fa16c3312
Jason Prince <jasoncprince@gmail.com>
2020-02-04 15:39:01 -0700

    fast move api calls created, not tested

--- player.py
+++ player.py
@@ -52,7 +52,9 @@ class Player:
 
     def sell(self):
         endpoint = "/adv/sell/"
-        data = {"name": "treasure"}
+        data = {
+            "name": "treasure"
+        }
         res = requests.post(
             self.base_url + endpoint,
             headers=headers,
@@ -62,7 +64,10 @@ class Player:
 
     def sell_confirm(self):
         endpoint = "/adv/sell/"
-        data = {"name": "treasure", "confirm": "yes"}
+        data = {
+            "name": "treasure",
+            "confirm": "yes"
+        }
         res = requests.post(
             self.base_url + endpoint,
             headers=headers,
@@ -74,7 +79,9 @@ class Player:
     def move(self, direction):
         print(f'Direction: {direction}')
         endpoint = "/adv/move/"
-        data = {"direction": direction}
+        data = {
+            "direction": direction
+        }
         res = requests.post(
             self.base_url + endpoint,
             headers=headers,
@@ -103,12 +110,17 @@ class Player:
 #---------------------------STATUS AND EXAMINE---------------------------#
     def status(self):
         endpoint = "/adv/status/"
-        res = requests.post(self.base_url + endpoint, headers=headers)
+        res = requests.post(
+            self.base_url + endpoint,
+            headers=headers
+        )
         print(f'------- {res.text} STATUS')
 
     def examine(self):
         endpoint = "/adv/examine/"
-        data = {"name": "Wishing Well"}
+        data = {
+            "name": "Wishing Well"
+        }
         res = requests.post(
             self.base_url + endpoint,
             headers=headers,
@@ -118,31 +130,64 @@ class Player:
 
 #---------------------------EQUIPMENT (WEAR AND UNDRESS)---------------------------#
     def wear(self, item):
+        endpoint = "/adv/wear/"
         data = {"name": item}
         res = requests.post(
-            'https://lambda-treasure-hunt.herokuapp.com/api/adv/wear/', headers=headers, data=json.dumps(data)
+            self.base_url + endpoint,
+            headers=headers,
+            data=json.dumps(data)
         )
         print(f'------- {res.text} WEAR')
 
     def undress(self, item):
+        endpoint = "/adv/undress/"
         data = {"name": item}
         res = requests.post(
-            'https://lambda-treasure-hunt.herokuapp.com/api/adv/undress/', headers=headers, data=json.dumps(data)
+            self.base_url + endpoint,
+            headers=headers,
+            data=json.dumps(data)
         )
         print(f'------- {res.text} UNDRESS')
 
 #---------------------------NAME CHANGER---------------------------#
     def change_name(self, name):
-        data = {"name": name}
+        endpoint = "/adv/change_name/"
+        data = {
+            "name": name
+        }
         res = requests.post(
-            'https://lambda-treasure-hunt.herokuapp.com/api/adv/change_name/', headers=headers, data=json.dumps(data)
+            self.base_url + endpoint,
+            headers=headers,
+            data=json.dumps(data)
         )
         print(f'------- {res.text} CHANGE NAME')
 
 #---------------------------FAST MOVE---------------------------#
-#     def dash(self, direction, number_of_rooms, sequential_room_ids):
-#         data = {"direction":"n", "num_rooms":"5", "next_room_ids":"10,19,20,63,72"
-# }
+    def dash(self, direction, number_of_rooms, sequential_room_ids):
+        endpoint = "/adv/dash/"
+        data = {
+            "direction": direction,
+            "num_rooms": number_of_rooms,
+            "next_room_ids": sequential_room_ids
+        }
+        res = requests.post(
+            self.base_url + endpoint,
+            headers=headers,
+            data=json.dumps(data)
+        )
+        print(f'------- {res.text} DASH')
+
+    def flight(self, direction):
+        endpoint = "/adv/fly/"
+        data = {
+            "direction": direction
+        }
+        res = requests.post(
+            self.base_url + endpoint,
+            headers=headers,
+            data=json.dumps(data)
+        )
+        print(f'------- {res.text} FLY')
 
 
 jason = Player("Jason", 0)
