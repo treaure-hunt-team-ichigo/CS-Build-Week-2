b57feafb0d0ce0a7dcc83c890390c0e1771ce3aa
jcrn <clemons.jason@gmail.com>
2020-02-04 13:28:38 -0800

    update

---
+++ traversal.py
@@ -0,0 +1,11 @@
+from player import Player
+
+player = Player.init
+
+def Traversal(player, ):
+    # Create a path list (p) - holds the cardinal directions
+    p = []
+
+    # Create a room stack (s) and add the starting room
+    s = []
+    s.append()
